faf24cbad45df9d61dabc0628595a57f923e6cd7
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-05-21 00:15:35 +0700

    learn : Multi Bloc Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learn_flutter/app.dart';
 import 'package:learn_flutter/bloc/counterBloc.dart';
 import 'package:learn_flutter/bloc/themeBloc.dart';
-import 'package:learn_flutter/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,20 +13,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => Themebloc(),
-      child: BlocBuilder<Themebloc, bool>(
-        builder: (context, state) {
-          return MaterialApp(
-            theme: (state) ? ThemeData.light() : ThemeData.dark(),
-            debugShowCheckedModeBanner: false,
-            home: BlocProvider(
-              create: (context) => Counterbloc(),
-              child: const HomePage(),
-            ),
-          );
-        },
+    return MultiBlocProvider(providers: [
+      BlocProvider(
+        create: (context) => Themebloc(),
       ),
-    );
+      BlocProvider(
+        create: (context) => Counterbloc(),
+      ),
+    ], child: App());
   }
 }
